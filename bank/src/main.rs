fb3448fa77f6d1ec5342f152460030aaab412b32
Bode <bode.falade@outlook.com>
2024-09-10 23:57:13 -0500

    add new impl method to Bank impl.

--- bank/src/main.rs
+++ bank/src/main.rs
@@ -24,27 +24,18 @@ impl Bank {
     fn new() -> Self {
         Bank { accounts: vec![] }
     }
+
+    fn add_account(&mut self, account: Account) {
+        self.accounts.push(account)
+    }
 }
-
-fn print_account(account: &Account) {
-    print!("{:#?}", account);
-}
-
-// I can uodate the value using ref mut
-fn change_account(account: &mut Account) {
-    account.balance = 10;
-
-}
-
 
 fn main() {
 
-    // some types of values are copied instead of moved. 
-    // numbers, bool, char, arrays, tuples, references are types that are copied and not moved
-   let num = 5;
+let mut bank = Bank::new();
+let account = Account::new(1, String::from("me"));
 
-   let other_num = num;
- 
-   println!("{:#?}", num)
+bank.add_account(account);
 
+println!("{:#?}", bank)
 }
