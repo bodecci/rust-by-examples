d4116f2b94499f35f2cf46737ae662bd411fe7a5
Bode <bode.falade@outlook.com>
2024-09-11 21:05:53 -0500

    using if let for pattern matching.

--- media/src/main.rs
+++ media/src/main.rs
@@ -6,6 +6,23 @@ enum Media {
     Audiobook { title: String },
 }
 
+impl Media {
+    fn description(&self) -> String {
+        // If self is a Media::Book, we can use title and author here
+        if let Media::Book { title, author } = self {
+            format!("Book: {} {}", title, author)
+            // If self is a Media::Movie, we can use title and director here
+        } else if let Media::Movie{ title, director } = self {
+            format!("Movie: {} {}", title, director)
+            // If self is a Media::Audiobok, we can use title and director here
+        } else if let Media::Audiobook {title} = self {
+            format!("Audiobook: {}", title)
+        } else {
+            String::from("Media Description")   
+        }
+    }
+}
+
 fn print_media(media: Media) {
     println!("{:#?}", media)
 }
@@ -22,6 +39,10 @@ fn main() {
         title: String::from("Bad Book"),
         author: String::from("Bad Author"), 
     };
+
+    println!("{}", audiobook.description());
+    println!("{}", good_movie.description());
+    println!("{}", bad_book.description());
 
     print_media(audiobook);
     print_media(good_movie);
