ef75e036c7429e88c3c54b3c6d955bcaef73fe13
Bode <bode.falade@outlook.com>
2024-09-11 21:17:12 -0500

    use pattern matching with match.

--- media/src/main.rs
+++ media/src/main.rs
@@ -8,17 +8,30 @@ enum Media {
 
 impl Media {
     fn description(&self) -> String {
-        // If self is a Media::Book, we can use title and author here
-        if let Media::Book { title, author } = self {
-            format!("Book: {} {}", title, author)
-            // If self is a Media::Movie, we can use title and director here
-        } else if let Media::Movie{ title, director } = self {
-            format!("Movie: {} {}", title, director)
-            // If self is a Media::Audiobok, we can use title and director here
-        } else if let Media::Audiobook {title} = self {
-            format!("Audiobook: {}", title)
-        } else {
-            String::from("Media Description")   
+        // // If self is a Media::Book, we can use title and author here
+        // if let Media::Book { title, author } = self {
+        //     format!("Book: {} {}", title, author)
+        //     // If self is a Media::Movie, we can use title and director here
+        // } else if let Media::Movie{ title, director } = self {
+        //     format!("Movie: {} {}", title, director)
+        //     // If self is a Media::Audiobok, we can use title and director here
+        // } else if let Media::Audiobook {title} = self {
+        //     format!("Audiobook: {}", title)
+        // } else {
+        //     String::from("Media Description")   
+        // }
+        match self{
+            // use pattern matching with match. This allows you to check which variant 
+            // of the enum you're dealing with and extract the associated data 
+            Media::Book { title, author } => {
+                format!("Book: {} {}", title, author)
+            }
+            Media::Movie { title, director } => {
+                format!("Movie: {} {}", title, director)
+            }
+            Media::Audiobook { title } => {
+                format!("Audiobook: {}", title)
+            }
         }
     }
 }
