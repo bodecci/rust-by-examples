fcfda9e8559d9d48125dab5e9109e10bdbf38097
Bode <bode.falade@outlook.com>
2024-09-12 20:28:11 -0500

    added more variants to the enum.

--- media/src/main.rs
+++ media/src/main.rs
@@ -4,6 +4,8 @@ enum Media {
     Book { title: String, author: String },
     Movie { title: String, director: String },
     Audiobook { title: String },
+    Podcast (u32),
+    Placeholder
 }
 
 impl Media {
@@ -31,6 +33,12 @@ impl Media {
             }
             Media::Audiobook { title } => {
                 format!("Audiobook: {}", title)
+            }
+            Media::Podcast(id) => {
+                format!("Podcast: {}", id)
+            }
+            Media::Placeholder => {
+                format!("Placeholder")
             }
         }
     }
@@ -67,6 +75,8 @@ fn main() {
         title: String::from("Bad Book"),
         author: String::from("Bad Author"), 
     };
+    let podcast = Media::Podcast(10);
+    let placeholder = Media::Placeholder;
 
     // println!("{:#?}", audiobook.description());
     // println!("{:#?}", good_movie.description());
@@ -77,6 +87,8 @@ fn main() {
     catalog.add(audiobook);
     catalog.add(good_movie);
     catalog.add(bad_book);
+    catalog.add(podcast);
+    catalog.add(placeholder);
 
     println!("{:#?}", catalog)
 
